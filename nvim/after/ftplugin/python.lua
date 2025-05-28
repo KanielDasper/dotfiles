40bcb8b403d7b837b5b07e60be198f7cae866e0b
KanielDasper <dkdanielkasper@gmail.com>
2025-05-28 20:40:13 +0200

    add python f-string plugin + ftplugin settings
    Signed-off-by: KanielDasper <dkdanielkasper@gmail.com>

--- nvim/after/ftplugin/python.lua
+++ nvim/after/ftplugin/python.lua
@@ -1,6 +1,18 @@
-local set = vim.opt_local
+-- Python specific indentations
+vim.opt_local.tabstop = 4
+vim.opt_local.shiftwidth = 4
+vim.opt_local.softtabstop = 4
+vim.opt_local.expandtab = true
 
-set.tabstop = 4
-set.shiftwidth = 4
-set.softtabstop = 4
-set.expandtab = true
+local function iabbrev(lhs, rhs)
+	vim.keymap.set("ia", lhs, rhs, { buffer = true })
+end
+
+-- Python specific abbreviations
+iabbrev("true", "True")
+iabbrev("false", "False")
+iabbrev("--", "#")
+iabbrev("null", "None")
+iabbrev("none", "None")
+iabbrev("nil", "None")
+iabbrev("function", "def")
---
+++ nvim/lua/opts/plugins/puppeteer.lua
@@ -0,0 +1,5 @@
+-- Converts python strings to f-strings automatically when {} is typed in the string
+return {
+	"chrisgrieser/nvim-puppeteer",
+	lazy = false, -- plugin lazy-loads itself. Can also load on filetypes.
+}
