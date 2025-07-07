348e37539d3b8412be8f574070f043c188d28397
KanielDasper <dkdanielkasper@gmail.com>
2025-07-07 10:37:38 +0200

    small fixes to lazydev and blink

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -1,22 +1,18 @@
 return {
 	"saghen/blink.cmp",
 	dependencies = { "rafamadriz/friendly-snippets" },
-
-	-- use a release tag to download pre-built binaries
-	-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-	-- build = 'cargo build --release',
 	version = "*",
 	opts = {
 		-- See :h blink-cmp-config-keymap for defining your own keymap
 		keymap = { preset = "default" },
+
 		appearance = {
 			nerd_font_variant = "mono",
 		},
-		-- Default list of enabled providers defined so that you can extend it
-		-- elsewhere in your config, without redefining it, due to `opts_extend`
 		completion = {
-			documentation = { auto_show = true, auto_show_delay_ms = 100, window = { border = "rounded" } },
+			documentation = { auto_show = true, auto_show_delay_ms = 0, window = { border = "rounded" } },
 			menu = { border = "rounded" },
+			keyword = { range = "full" },
 		},
 		sources = {
 			default = { "lazydev", "lsp", "path", "snippets", "buffer" },
@@ -29,7 +25,6 @@ return {
 				},
 			},
 		},
-
 		fuzzy = { implementation = "prefer_rust_with_warning" }, -- See the fuzzy documentation for more information
 	},
 	opts_extend = { "sources.default" },
