e0da64738f8ac624df15f78ab7364fb4440fdede
KanielDasper <dkdanielkasper@gmail.com>
2025-08-30 01:19:38 +0200

    Neowiki config and plugin update

--- nvim/lua/plugins/neowiki.lua
+++ nvim/lua/plugins/neowiki.lua
@@ -13,11 +13,15 @@ return {
 				border = "rounded",
 			},
 		},
+		keymaps = {
+			navigate_back = "<Backspace>",
+			jump_to_index = "<Esc>",
+		},
 	},
 	keys = {
 		-- See https://github.com/echaya/neowiki.nvim for more keymaps
-		{ "<leader>ww", "<cmd>lua require('neowiki').open_wiki_floating()<cr>", desc = "Open Wiki in Floating Window" },
-		{ "<leader>wW", "<cmd>lua require('neowiki').open_wiki()<cr>", desc = "Open Wiki" },
-		{ "<leader>wT", "<cmd>lua require('neowiki').open_wiki_new_tab()<cr>", desc = "Open Wiki in Tab" },
+		-- { "<leader>ww", "<cmd>lua require('neowiki').open_wiki_floating()<cr>", desc = "Open Wiki in Floating Window" },
+		{ "<leader>W", "<cmd>lua require('neowiki').open_wiki()<cr>", desc = "Open Wiki" },
+		-- { "<leader>wT", "<cmd>lua require('neowiki').open_wiki_new_tab()<cr>", desc = "Open Wiki in Tab" },
 	},
 }
