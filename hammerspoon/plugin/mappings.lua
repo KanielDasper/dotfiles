664a644acc02857ce0414db363cfbdf7033e243e
KanielDasper <dkdanielkasper@gmail.com>
2026-03-02 17:28:54 +0100

    chore: remove unused mappings and cleanup

--- hammerspoon/plugin/mappings.lua
+++ hammerspoon/plugin/mappings.lua
@@ -1,4 +1,3 @@
--- mappings.lua
 hs.hotkey.bind("ctrl", "8", function()
 	hs.eventtap.keyStrokes("[")
 end)
@@ -24,30 +23,11 @@ end)
 -- Nightlight lumen, timeStart, timeEnd
 hs.redshift.start(2000, "06:00", "05:00")
 hs.hotkey.bind({ "cmd" }, "$", "Toggle Redshift", hs.redshift.toggle)
-
--- Move to center of screen
-local function resizeAndCenterWindow()
-	local win = hs.window.focusedWindow()
-	if win then
-		local screen = win:screen()
-		local frame = screen:frame()
-		-- Change values to adjust screen size
-		frame.w = frame.w / 1.1
-		frame.h = frame.h / 1.1
-		win:setFrame(frame)
-		win:centerOnScreen(nil, true, 0.1)
-	end
-end
-
-hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "f", function()
-	resizeAndCenterWindow()
+hs.hotkey.bind({ "ctrl", "cmd" }, "t", function()
+	hs.execute("open ~/.Trash", true)
 end)
 hs.hotkey.bind({ "ctrl", "cmd" }, "f", function()
 	hs.application.launchOrFocus("Finder")
-end)
-hs.hotkey.bind({ "ctrl", "cmd" }, "t", function()
-	hs.execute("open ~/.Trash", true)
-	resizeAndCenterWindow()
 end)
 hs.hotkey.bind("ctrl", "return", function()
 	hs.application.launchOrFocus("Ghostty")
